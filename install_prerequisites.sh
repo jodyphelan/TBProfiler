a99547d102e36c989b35d67419d31c90c9aca6fd
Jody Phelan <jody.phelan@lshtm.ac.uk>
2016-12-27 20:28:25 +0000

    p

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -1,12 +1,16 @@
+mkdir bin
 cd htsbox
 make
-mv htsbox ../bin
+mv htsbox ../bin/
 cd ../
 cd htslib
 make
-mv tabix bgzip ../bin
+mv tabix bgzip ../bin/
+cd ../
+cd snap
+make
+mv snap-aligner ../bin/
 cd ../
 wget https://github.com/lomereiter/sambamba/releases/download/v0.6.5/sambamba_v0.6.5_linux.tar.bz2
 tar -xvf sambamba_v0.6.5_linux.tar.bz2
 mv sambamba_v0.6.5 bin/
-
