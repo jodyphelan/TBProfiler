cf2b771ff11a625b2220b148a0a51e21a4c3e1fa
Jody Phelan <jody.phelan@lshtm.ac.uk>
2016-12-27 20:53:31 +0000

    p

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -14,3 +14,5 @@ cd ../
 wget https://github.com/lomereiter/sambamba/releases/download/v0.6.5/sambamba_v0.6.5_linux.tar.bz2
 tar -xvf sambamba_v0.6.5_linux.tar.bz2
 mv sambamba_v0.6.5 bin/sambamba
+wget http://pathogenseq.lshtm.ac.uk/downloads/TBProfilerFiles.tgz
+tar -xvf TBProfilerFiles.tgz
