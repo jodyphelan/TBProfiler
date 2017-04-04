eaa5b67f12e1b4091b5e27bbc94629038413542e
jody <jody@jodys-MacBook-Pro.local>
2017-04-04 08:05:13 +0100

    added bwa support

--- osX_install_prerequisites.sh
+++ osX_install_prerequisites.sh
@@ -7,9 +7,10 @@ cd htslib
 make
 mv tabix bgzip ../bin/
 cd ../
-curl http://snap.cs.berkeley.edu/downloads/snap-1.0beta.1-mac.zip > snap.zip
-unzip snap.zip 
-mv snap-1.0beta.1-mac/snap bin/snap-aligner
+cd bwa
+make
+mv bwa ../bin
+cd ../
 wget https://github.com/lomereiter/sambamba/releases/download/v0.6.6/sambamba_v0.6.6_osx.tar.bz2 --no-check-certificate
 tar -xvf sambamba_v0.6.6_osx.tar.bz2 
 mv sambamba_v0.6.6 bin/sambamba
