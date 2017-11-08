b3f4806b6171999755d70cdca56015768d9758e9
Jody Phelan <jody.phelan@lshtm.ac.uk>
2017-11-08 01:46:47 +0000

    v0.3

---
+++ osX_install_prerequisites.sh
@@ -0,0 +1,46 @@
+#minimap2
+cd minimap2
+make
+mv minimap2 ../bin
+cd ../
+
+#htsbox
+cd htsbox
+make
+mv htsbox ../bin
+cd ../
+
+#htslib
+cd htslib
+autoheader
+autoconf
+./configure --disable-lzma;make
+mv tabix bgzip ../bin/
+cd ../
+
+#bwa
+cd bwa
+make
+mv bwa ../bin
+cd ../
+
+#bcftools
+cd bcftools
+make
+mv bcftools ../bin
+cd ../
+
+#samtools
+cd samtools
+make
+mv samtools ../bin
+cd ../
+
+#lofreq
+cd lofreq/dist/
+tar -xvf lofreq_star-2.1.3.1_macosx.tgz
+mv lofreq_star-2.1.3.1/bin/lofreq ../../bin/
+cd ../../
+
+wget http://pathogenseq.lshtm.ac.uk/downloads/TBProfilerFiles.tgz
+tar -xvf TBProfilerFiles.tgz
