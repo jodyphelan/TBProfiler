d0d6045d8a241f1e8a37a91029243beea2d4049b
Jody Phelan <jody.phelan@lshtm.ac.uk>
2018-01-22 11:43:51 +0000

    bowtie threads

--- tbprofiler/mapping.py
+++ tbprofiler/mapping.py
@@ -8,9 +8,9 @@ def map(self):
 				cmd = "%(bwa)s mem -R '@RG\\tID:%(prefix)s\\tSM:%(prefix)s\\tPL:%(platform)s' -t %(threads)s %(reffile)s %(fq1)s | %(samtools)s view -b -@ %(threads)s - | %(samtools)s sort -@ %(threads)s -o %(bamfile)s -" % self.params
 		elif self.mapper == "bowtie2":
 			if self.params["fq1"] and self.params["fq2"]:
-				cmd = "%(bowtie2)s -x %(reffile)s --rg-id %(prefix)s --rg '@RG\\tID:%(prefix)s\\tSM:%(prefix)s\\tPL:%(platform)s' -1 %(fq1)s -2 %(fq2)s | samtools view -@ %(threads)s -b - | samtools sort -@ %(threads)s -o %(bamfile)s -" % self.params
+				cmd = "%(bowtie2)s -p %(threads)s -x %(reffile)s --rg-id %(prefix)s --rg '@RG\\tID:%(prefix)s\\tSM:%(prefix)s\\tPL:%(platform)s' -1 %(fq1)s -2 %(fq2)s | samtools view -@ %(threads)s -b - | samtools sort -@ %(threads)s -o %(bamfile)s -" % self.params
 			if self.params["fq1"]:
-				cmd = "%(bowtie2)s -x %(reffile)s --rg-id %(prefix)s --rg '@RG\\tID:%(prefix)s\\tSM:%(prefix)s\\tPL:%(platform)s' -U %(fq1)s | samtools view -@ %(threads)s -b - | samtools sort -@ %(threads)s -o %(bamfile)s -" % self.params
+				cmd = "%(bowtie2)s -p %(threads)s -x %(reffile)s --rg-id %(prefix)s --rg '@RG\\tID:%(prefix)s\\tSM:%(prefix)s\\tPL:%(platform)s' -U %(fq1)s | samtools view -@ %(threads)s -b - | samtools sort -@ %(threads)s -o %(bamfile)s -" % self.params
 	elif self.params["platform"] == "minION":
 		cmd = "%(minimap2)s -ax map-ont -t %(threads)s %(reffile)s %(fq1)s  | %(samtools)s view -@ %(threads)s -b -  | %(samtools)s sort -@ %(threads)s -o %(bamfile)s - " % self.params
 	else:
