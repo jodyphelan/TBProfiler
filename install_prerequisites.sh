a22fb58db530c377fcb44074896832daa3395473
Jody Phelan <jody.phelan@lshmt.ac.uk>
2018-05-17 15:29:47 +0100

    stop install script when error occurs

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -5,14 +5,14 @@ cd minimap2 \
  && make \
  && cp minimap2 ../bin \
  && cd ../ \
- && echo minimap2 OK || echo minimap2 FAIL
+ && echo minimap2 OK || exit 1
 
 #htsbox
 cd htsbox \
  && make \
  && mv htsbox ../bin \
  && cd ../ \
- && echo htsbox OK || echo htsbox FAIL
+ && echo htsbox OK || exit 1
 
 #htslib
 cd htslib \
@@ -21,43 +21,47 @@ cd htslib \
  && ./configure --disable-lzma;make \
  && mv tabix bgzip ../bin/ \
  && cd ../ \
- && echo htslib OK || echo htslib FAIL
+ && echo htslib OK || exit 1
 
 #bwa
 cd bwa \
  && make \
  && mv bwa ../bin \
  && cd ../ \
- && echo bwa OK || echo bwa FAIL
+ && echo bwa OK || exit 1
+
 #bowtie2
 wget -O bowtie2-2.3.4-linux-x86_64.zip https://github.com/BenLangmead/bowtie2/releases/download/v2.3.4/bowtie2-2.3.4-linux-x86_64.zip \
  && unzip bowtie2-2.3.4-linux-x86_64.zip \
  && cd bowtie2-2.3.4-linux-x86_64 \
  && mv bowtie2* ../bin \
  && cd ../ \
- && echo bowtie2 OK || echo bowtie2 FAIL
+ && echo bowtie2 OK || exit 1
+
 #bcftools
 cd bcftools \
  && make \
  && mv bcftools ../bin \
  && cd ../ \
- && echo bcftools OK || echo bcftools FAIL
+ && echo bcftools OK || exit 1
+
 #samtools
 cd samtools \
  && make \
  && mv samtools ../bin \
  && cd ../ \
- && echo samtools OK || echo samtools FAIL
+ && echo samtools OK || exit 1
 
 #lofreq
 cd lofreq/dist/ \
  && tar -xvf lofreq_star-2.1.3.1_linux-x86-64.tgz \
  && mv lofreq_star-2.1.3.1/bin/lofreq ../../bin/ \
  && cd ../../ \
- && echo lofreq OK || echo lofreq FAIL
+ && echo lofreq OK || exit 1
 
 wget http://pathogenseq.lshtm.ac.uk/downloads/TBProfilerFiles.tgz \
  && tar -xvf TBProfilerFiles.tgz \
  && ./bin/bowtie2-build ref/MTB-h37rv_asm19595v2-eg18.fa ref/MTB-h37rv_asm19595v2-eg18.fa \
- && echo Files OK || echo Files FAIL
+ && echo Files OK || exit 1
+ 
 python scripts/create_config.py
