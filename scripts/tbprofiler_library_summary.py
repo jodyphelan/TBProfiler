c3b8099318c68160773733b60cf607518cfcbdf9
Jody Phelan <jody.phelan@lshmt.ac.uk>
2019-03-25 20:47:36 +0000

    change reformat of promoter mutations

--- scripts/tbprofiler_library_summary.py
+++ scripts/tbprofiler_library_summary.py
@@ -55,6 +55,8 @@ def main(args):
 	for drug in drugs:
 		for locus in sorted(lib[drug]):
 			if args.ngs:
+				print(variants[locus])
+				print(lib[drug][locus]["snps"])
 				num_snps_ngs = sum([1 if v in variants[locus] else 0 for v in lib[drug][locus]["snps"]])
 				num_indels_ngs = sum([1 if v in variants[locus] else 0 for v in lib[drug][locus]["indels"]])
 				print("%s\t%s\t%s\t%s (%s)\t%s (%s)" % (drug,locus,rv2gene[locus],len(lib[drug][locus]["snps"]),num_snps_ngs,len(lib[drug][locus]["indels"]),num_indels_ngs))
