498450d2efe8136b1deecce484cee1f94a7b59d8
Jody Phelan <jody.phelan@lshmt.ac.uk>
2019-04-02 19:37:39 +0100

    add CSV output

--- tbprofiler/html.py
+++ tbprofiler/html.py
@@ -14,7 +14,7 @@ def load_html(html_strings):
 	return r"""
 <p>
 <p>
-<a href="input/results/%(id)s.results.csv">Download results <img src="/img/download_icon.jpeg"></img></a>
+<a href="input/results/%(id)s.results.csv">Download results <img src="/img/download_icon.jpeg" height="50px" width="50px"></img></a>
 <div><b>Summary</b></div>
 <div><b>ID:</b> %(id)s</div>
 <div><b>Date:</b> %(date)s</div>
