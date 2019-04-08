340114311d958a4dc05553666b335bb862f302ec
Jody Phelan <jody.phelan@lshmt.ac.uk>
2019-04-08 14:48:23 +0100

    get_library_freq script

--- scripts/tbprofiler_get_dr_freq.py
+++ scripts/tbprofiler_get_dr_freq.py
@@ -4,6 +4,7 @@ import sys
 from collections import defaultdict
 import re
 import argparse
+from collections import defaultdict
 
 def main(args):
 	drugs = [
@@ -13,8 +14,9 @@ def main(args):
 	'delamanid', 'bedaquiline', 'linezolid', ]
 	if args.meta:
 		meta = json.load(open(args.meta))
-		col,val = args.meta_col.split(",")
-		meta_samples = [s for s in meta if mets[s][col]==val]
+		samples2meta = {s:meta[s][args.meta_col] for s in meta}
+		meta_cats = {c:0 for x in samples2meta}
+		meta_cats["NA"] = 0
 	variants = defaultdict(lambda:defaultdict(int))
 	data = json.load(open(sys.argv[1]))
 	for s in data:
@@ -24,13 +26,25 @@ def main(args):
 			muts = [x.strip() for x in data[s][d].split(",")]
 			for m in muts:
 				variants[d][m]+=1
+				if args.meta:
+					if s in samples2meta:
+						meta_cats[meta2samples[s]]+=1
+					else:
+						meta_cats["NA"]+=1
 
 	for d in drugs:
 		for m in variants[d]:
 			re_obj = re.search("([a-zA-Z0-9]+)_(.*)",m)
 			gene = re_obj.group(1)
 			var = re_obj.group(2)
-			print("%s\t%s\t%s\t%s" % (d.capitalize(),gene,var,variants[d][m]))
+			sys.stdout.write("%s\t%s\t%s\t%s" % (d.capitalize(),gene,var,variants[d][m]))
+			if args.meta:
+				for cat in meta_cats:
+					num = meta_cats[cat]
+					tot_num = len([x for x in samples2meta if samples2meta[x]==cat])
+					pct = num/tot_num*100
+					sys.stdout.write("\t%s/%s (%.2f)" % (num,tot_num,pct))
+			sys.stdout.write("\n")
 
 parser = argparse.ArgumentParser(description='TBProfiler pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('json',help='NGS Platform')
