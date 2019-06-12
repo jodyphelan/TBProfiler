5920a978ff069872b0d32678d66d4a53fe586f19
Jody Phelan <jody.phelan@lshmt.ac.uk>
2019-06-12 15:19:14 +0100

    change variant matrix script

--- scripts/tbprofiler_variant_matrix.py
+++ scripts/tbprofiler_variant_matrix.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 
 def main(args):
 	if args.samples:
-		samples = [x.rstrip() for x in open(args.sample_file).readlines()]
+		samples = [x.rstrip() for x in open(args.samples).readlines()]
 	else:
 		samples = [x.replace(".results.json","") for x in os.listdir("%s/"%args.dir) if x[-13:]==".results.json"]
 	mutations = defaultdict(set)
@@ -18,17 +18,23 @@ def main(args):
 			mutations[(var["gene"],var["change"])].add(s)
 		for var in tmp["other_variants"]:
 			mutations[(var["gene"],var["change"])].add(s)
-	O = open(args.out,"w") if args.out else sys.stdout
+	O = open(args.txt,"w") if args.txt else sys.stdout
 	O.write("gene\tchange\t%s\n" % ("\t".join(samples)))
 	for gene,change in tqdm(mutations):
 		O.write("%s\t%s\t%s\n" % (gene,change,"\t".join(["1" if s in mutations[(gene,change)] else "0" for s in samples])))
 	O.close()
+	if args.fasta:
+		F = open(args.fasta,"w")
+		for s in tqdm(samples):
+			F.write(">%s\n%s\n" % (s, "".join(["1" if s in mutations[(gene,change)] else "0" for gene,change in mutations])))
+		F.close()
 
 
 parser = argparse.ArgumentParser(description='TBProfiler pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--samples',type=str,help='NGS Platform')
 parser.add_argument('--dir',default="results/",type=str,help='NGS Platform')
-parser.add_argument('--out',default="results/",type=str,help='NGS Platform')
+parser.add_argument('--txt',type=str,help='NGS Platform')
+parser.add_argument('--fasta',type=str,help='NGS Platform')
 parser.set_defaults(func=main)
 
 args = parser.parse_args()
