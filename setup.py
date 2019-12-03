4fae86167d5bfb090de3ae634fc658a04384e842
Jody Phelan <jody.phelan@lshmt.ac.uk>
2019-12-03 14:04:46 +0000

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
 
 	name="tbprofiler",
-	version="2.7.3",
+	version="2.7.4",
 	packages=["tbprofiler",],
 	license="MIT",
 	long_description="TBProfiler command line tool",
