a8a0e0aae4c56abfa52b3b6f2f46aa197a589a34
jodyphelan <jody.phelan@lshtm.ac.uk>
2019-12-12 22:04:54 +0000

    update version and remove deprecated modules

--- tbprofiler/__init__.py
+++ tbprofiler/__init__.py
@@ -2,8 +2,6 @@ from .tex import *
 from .text import *
 from .reformat import *
 from .collate import *
-from .phylogeny import *
-from .vcfprofile import *
 import os
 _ROOT = os.path.abspath(os.path.dirname(__file__))
-_VERSION = "2.7.4"
+_VERSION = "2.8.0"
