ba353249944a38f42cd91d70d794055a24c2165a
Jody Phelan <jody.phelan@lshmt.ac.uk>
2020-02-03 08:15:43 +0000

    gene_profiler script update

--- scripts/tbprofiler_gene_profiler.py
+++ scripts/tbprofiler_gene_profiler.py
@@ -17,7 +17,7 @@ def get_conf_dict(library_prefix):
     return conf
 
 
-def main(args):
+def main_profile(args):
     if args.external_db:
         conf = get_conf_dict(args.external_db)
     else:
@@ -35,16 +35,39 @@ def main(args):
     json.dump(results,open(outfile,"w"))
     pp.run_cmd("rm %(prefix)s.targets.vcf.gz* %(prefix)s.targets.csq.vcf.gz*" % vars(args))
 
+
+def main_collate(args):
+    samples = [x.replace(args.suffix,"") for x in os.listdir(args.dir) if x[-len(args.suffix):]==args.suffix]
+    variants = defaultdict(lambda:defaultdict(set))
+    for s in samples:
+        j = json.load(open("%s/%s.%s" % (args.dir,s,args.suffix)))
+        print("%s\t%s" % (s,";".join([var["change"] for var in j["variants"]])))
+        for var in j["variants"]:
+            variants[var["locus_tag"]][var["change"]].add(s)
+
 parser = argparse.ArgumentParser(description='TBProfiler pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--bam',type=str,help='NGS Platform',required=True)
-parser.add_argument('--prefix',type=str,help='Sample prefix for all results generated')
-parser.add_argument('--suffix',type=str,default=".results.json",help='Output file suffix')
-parser.add_argument('--platform','-m',choices=["illumina","nanopore"],default="illumina",help='NGS Platform used to generate data')
-parser.add_argument('--caller',default="bcftools", choices=["bcftools","gatk"],help="Variant calling tool to use.",type=str)
-parser.add_argument('--db',default='tbdb',help='Mutation panel name')
-parser.add_argument('--external_db',type=str,help='Path to db files prefix (overrides "--db" parameter)')
-parser.add_argument('--threads','-t',default=1,help='Threads to use',type=int)
-parser.set_defaults(func=main)
+subparsers = parser.add_subparsers(help="Task to perform")
+
+
+
+parser_sub = subparsers.add_parser('profile', help='(BETA) Run profiling pipeline on Fasta file. Warning: this assumes that this is a good quality assembly which coveres all drug resistance loci', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser_sub.add_argument('--bam',type=str,help='BAM file',required=True)
+parser_sub.add_argument('--prefix',type=str,help='Sample prefix for all results generated')
+parser_sub.add_argument('--suffix',type=str,default=".results.json",help='Output file suffix')
+parser_sub.add_argument('--platform','-m',choices=["illumina","nanopore"],default="illumina",help='NGS Platform used to generate data')
+parser_sub.add_argument('--caller',default="bcftools", choices=["bcftools","gatk"],help="Variant calling tool to use.",type=str)
+parser_sub.add_argument('--db',default='tbdb',help='Mutation panel name')
+parser_sub.add_argument('--external_db',type=str,help='Path to db files prefix (overrides "--db" parameter)')
+parser_sub.add_argument('--threads','-t',default=1,help='Threads to use',type=int)
+parser_sub.set_defaults(func=main_profile)
+
+parser_sub = subparsers.add_parser('collate', help='Output program version and exit', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser_sub.add_argument('--dir',type=str,default=".",help='Result directory')
+parser_sub.add_argument('--suffix',type=str,default=".results.json",help='Output file suffix')
+parser_sub.set_defaults(func=main_collate)
 
 args = parser.parse_args()
-args.func(args)
+if vars(args)=={}:
+    parser.print_help(sys.stderr)
+else:
+    args.func(args)
