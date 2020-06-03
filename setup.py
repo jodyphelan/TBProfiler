3515d45c102cdcfb1ebb977634219ce31c8e1294
Jody Phelan <jody.phelan@lshtm.ac.uk>
2020-06-03 14:48:39 +0100

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
 
 	name="tbprofiler",
-	version="2.8.8",
+	version="2.8.9",
 	packages=["tbprofiler","pathogenprofiler"],
 	license="MIT",
 	long_description="TBProfiler command line tool",
