ecdf1f569cfc03f7af6940d96e4e261612dd88a6
Jody Phelan <jody.phelan@lshtm.ac.uk>
2020-06-26 09:26:39 +0100

    bugfix: Issue #117

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
 
 	name="tbprofiler",
-	version="2.8.10",
+	version="2.8.11",
 	packages=["tbprofiler","pathogenprofiler"],
 	license="MIT",
 	long_description="TBProfiler command line tool",
