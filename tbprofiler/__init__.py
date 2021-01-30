d9c39a4a35f6b5a17091ec9439ab3026fd2f00b1
Jody Phelan <jody.phelan@lshtm.ac.uk>
2021-01-30 01:28:27 +0000

    rm neo4j module

--- tbprofiler/__init__.py
+++ tbprofiler/__init__.py
@@ -3,7 +3,6 @@ from .text import *
 from .reformat import *
 from .collate import *
 from .utils import *
-from .neo4j import *
 import os
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 _VERSION = "3.0.0"
