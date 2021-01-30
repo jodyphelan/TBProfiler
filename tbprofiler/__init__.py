2f39728ae2ea82d2761022a15217d208bf3850e9
Jody Phelan <jody.phelan@lshtm.ac.uk>
2021-01-30 10:22:34 +0000

    add create_db function

--- tbprofiler/__init__.py
+++ tbprofiler/__init__.py
@@ -3,6 +3,7 @@ from .text import *
 from .reformat import *
 from .collate import *
 from .utils import *
+from .db import *
 import os
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 _VERSION = "3.0.0"
