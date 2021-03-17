14f45b75f870b75c031d45d865b8bca5da01010c
jodyphelan <jody.phelan@lshtm.ac.uk>
2021-03-17 15:51:53 +0000

    Fix for Issue: #143

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
 
 	name="tbprofiler",
 
-	version="3.0.1",
+	version="3.0.2",
 	packages=["tbprofiler","pathogenprofiler"],
 	license="MIT",
 	long_description="TBProfiler command line tool",
