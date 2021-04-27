cec811ca7b2e73ada6f7c8c37246d965689c00cc
jodyphelan <jody.phelan@lshtm.ac.uk>
2021-04-27 19:24:25 +0100

    add automated testing

---
+++ tests/run_test.py
@@ -0,0 +1,102 @@
+import subprocess as sp
+import json
+import pathogenprofiler as pp
+import os
+import tbprofiler as tbp
+import argparse
+import sys
+
+
+
+por5_dr_variants = [
+    ('rpoB', 'p.Ser450Leu'),
+    ('fabG1', 'c.-15C>T'),
+    ('inhA', 'p.Ile194Thr'),
+    ('pncA', 'p.Val125Gly'),
+    ('embB', 'p.Met306Val'),
+    ('embB', 'p.Met423Thr'),
+    ('gid', 'p.Ala80Pro')
+]
+
+    
+def test_vcf():
+    sp.call("tb-profiler vcf_profile /home/jody/tbprofiler_test_data/por5A1.vcf.gz",shell=True)
+    results = json.load(open("results/por5_vcf.results.json"))
+    assert results["sublin"] == "lineage4.3.4.2"
+    assert results["main_lin"] == "lineage4"
+    assert len(results["dr_variants"]) == 7
+    assert [(v["gene"],v["change"]) for v in results["dr_variants"]] == por5_dr_variants
+
+
+def illumina_fastq(caller,mapper):
+    sp.call(f"tb-profiler profile -1 /home/jody/tbprofiler_test_data/por5A.reduced_1.fastq.gz -2 /home/jody/tbprofiler_test_data/por5A.reduced_2.fastq.gz --mapper {mapper} --caller {caller} -p por5A_illumina_{caller} -t 3 --txt --csv --pdf",shell=True)
+    results = json.load(open(f"results/por5A_illumina_{caller}.results.json"))
+    return results
+
+def illumina_fastq_single(caller,mapper):
+    sp.call(f"tb-profiler profile -1 /home/jody/tbprofiler_test_data/por5A.reduced_1.fastq.gz --mapper {mapper} --caller {caller} -p por5A_illumina_{caller} -t 3 --txt --csv --pdf",shell=True)
+    results = json.load(open(f"results/por5A_illumina_{caller}.results.json"))
+    return results
+
+def test_bwa_freebayes():
+    results = illumina_fastq("freebayes","bwa")
+    assert results["sublin"] == "lineage4.3.4.2"
+    assert results["main_lin"] == "lineage4"
+    assert [(v["gene"],v["change"]) for v in results["dr_variants"]] == por5_dr_variants
+
+def test_bwa_bcftools():
+    results = illumina_fastq("bcftools","bwa")
+    assert results["sublin"] == "lineage4.3.4.2"
+    assert results["main_lin"] == "lineage4"
+    assert [(v["gene"],v["change"]) for v in results["dr_variants"]] == por5_dr_variants
+
+def test_bwa_gatk():
+    results = illumina_fastq("gatk","bwa")
+    assert results["sublin"] == "lineage4.3.4.2"
+    assert results["main_lin"] == "lineage4"
+    assert [(v["gene"],v["change"]) for v in results["dr_variants"]] == por5_dr_variants
+
+def test_bowtie2_freebayes():
+    results = illumina_fastq("freebayes","bowtie2")
+    assert results["sublin"] == "lineage4.3.4.2"
+    assert results["main_lin"] == "lineage4"
+    assert [(v["gene"],v["change"]) for v in results["dr_variants"]] == por5_dr_variants
+
+def test_minimap2_freebayes():
+    results = illumina_fastq("freebayes","minimap2")
+    assert results["sublin"] == "lineage4.3.4.2"
+    assert results["main_lin"] == "lineage4"
+    assert [(v["gene"],v["change"]) for v in results["dr_variants"]] == por5_dr_variants
+
+def test_bwa_freebayes_single():
+    results = illumina_fastq_single("freebayes","minimap2")
+    assert results["sublin"] == "lineage4.3.4.2"
+    assert results["main_lin"] == "lineage4"
+    assert [(v["gene"],v["change"]) for v in results["dr_variants"]] == por5_dr_variants
+
+def test_minimap2_freebayes():
+    results = illumina_fastq("freebayes","minimap2")
+    assert results["sublin"] == "lineage4.3.4.2"
+    assert results["main_lin"] == "lineage4"
+    assert [(v["gene"],v["change"]) for v in results["dr_variants"]] == por5_dr_variants
+
+def test_nanopore():
+    sp.call(f"tb-profiler profile -1 /home/jody/tbprofiler_test_data/por5A.nanopore_reduced.fastq.gz --platform nanopore -p por5A_illumina_nanopore",shell=True)
+    results = json.load(open(f"results/por5A_illumina_nanopore.results.json"))
+    assert results["sublin"] == "lineage4.3.4.2"
+    assert results["main_lin"] == "lineage4"
+    assert [(v["gene"],v["change"]) for v in results["dr_variants"]] == por5_dr_variants
+
+def test_fasta():
+    sp.call(f"tb-profiler fasta_profile -f ~/tbprofiler_test_data/por5A1.fasta  -p por5A_fasta",shell=True)
+    results = json.load(open(f"results/por5A_fasta.results.json"))
+    assert results["sublin"] == "lineage4.3.4.2"
+    assert results["main_lin"] == "lineage4"
+    assert [(v["gene"],v["change"]) for v in results["dr_variants"]] == por5_dr_variants
+
+def test_db():
+    sp.call("git clone https://github.com/jodyphelan/tbdb.git",shell=True)
+    os.chdir("tbdb")
+    sp.call("tb-profiler create_db",shell=True)
+    os.chdir("../")
+
