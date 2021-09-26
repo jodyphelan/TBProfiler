fe3603c6a7db5c10aaaf973524e02f4884e2e3cd
jodyphelan <jody.phelan@lshtm.ac.uk>
2021-09-26 19:18:11 +0100

    debug

--- pathogenprofiler/vcf.py
+++ pathogenprofiler/vcf.py
@@ -355,11 +355,13 @@ class delly_bcf(vcf):
          vcf.__init__(self,filename)
     def get_robust_calls(self):
         results = []
-        for l in cmd_out("bcftools query -f '%%CHROM\\t%%POS\\t[%%END\\t%%GT\\t%%DR\\t%%DV\\t%%RR\\t%%RV]\\n' %(filename)s" % vars(self)):
-            row = l.split()
-            if row[3]!="1/1":continue
-            if int(row[2])-int(row[1])>100000: continue
-            results.append(row)
+        with open(f"/tmp/{self.filename}.calls.txt","w") as LOG:
+            for l in cmd_out("bcftools query -f '%%CHROM\\t%%POS\\t[%%END\\t%%GT\\t%%DR\\t%%DV\\t%%RR\\t%%RV]\\n' %(filename)s" % vars(self)):
+                row = l.split()
+                if row[3]!="1/1":continue
+                if int(row[2])-int(row[1])>100000: continue
+                results.append(row)
+                LOG.write(l+"\n")
         return results
     def overlap_bed(self,bed_file):
         results = []
