abf0e1cac374cbc9bab72b9896aedba6def67a77
Jody Phelan <jody.phelan@lshtm.ac.uk>
2023-05-28 22:49:37 +0100

    add tb-profiler-tools

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
 	long_description="TBProfiler command line tool",
 	scripts= [
 		'tb-profiler',
+        'scripts/tb-profiler-tools'
 		],
 	data_files=[('share/tbprofiler',["db/default_template.docx","db/tbdb.mask.bed","db/tbdb.spoligotype_spacers.txt","db/tbdb.spoligotype_list.csv","db/tbdb.barcode.bed","db/tbdb.bed","db/tbdb.dr.json","db/tbdb.fasta","db/tbdb.gff","db/tbdb.version.json","db/tbdb.variables.json","example_data/tbprofiler.test.fq.gz"])]
 )
