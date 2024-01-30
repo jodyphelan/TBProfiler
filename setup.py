86ae6c6da62367b3760a3fa137a02ac8a41fa33e
Jody Phelan <jody.phelan@lshtm.ac.uk>
2024-01-30 20:09:21 +0000

    update setup

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+from glob import glob
 
 version = [l.strip() for l in open("tbprofiler/__init__.py") if "version" in l][0].split('"')[1]
 
@@ -14,5 +15,5 @@ setuptools.setup(
 		'tb-profiler',
         'scripts/tb-profiler-tools'
 		],
-	data_files=[('share/tbprofiler',["db/default_template.docx","db/tbdb.mask.bed","db/tbdb.spoligotype_spacers.txt","db/tbdb.spoligotype_list.csv","db/tbdb.barcode.bed","db/tbdb.bed","db/tbdb.dr.json","db/tbdb.fasta","db/tbdb.gff","db/tbdb.version.json","db/tbdb.variables.json","example_data/tbprofiler.test.fq.gz"])]
+	data_files=[('share/tbprofiler',[glob("db/*")])],
 )
