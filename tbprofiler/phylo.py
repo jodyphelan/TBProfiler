92a12bbe1594976c8601b5cd3fc21b26ee26cfb5
Jody Phelan <jody.phelan@lshtm.ac.uk>
2024-04-15 15:45:46 +0100

    fix for no bam

--- tbprofiler/phylo.py
+++ tbprofiler/phylo.py
@@ -72,8 +72,12 @@ def prepare_sample_consensus(sample: str,input_vcf: str,args: argparse.Namespace
     mask_bed = f"{args.files_prefix}.{s}.mask.bed"
     if hasattr(args,'supplementary_bam') and args.supplementary_bam:
         args.bam = args.supplementary_bam
-    generate_low_dp_mask(f"{args.bam}",args.conf['ref'],mask_bed)
-    run_cmd(f"bcftools consensus --sample {s} -m {mask_bed} -M N -f {args.conf['ref']} {tmp_vcf} | sed 's/>/>{s} /' > {args.files_prefix}.{s}.consensus.fa")
+    if args.bam:
+        generate_low_dp_mask(f"{args.bam}",args.conf['ref'],mask_bed)
+        mask_cmd = f"-m {mask_bed} -M N"
+    else:
+        mask_cmd = ""
+    run_cmd(f"bcftools consensus --sample {s} {mask_cmd} -f {args.conf['ref']} {tmp_vcf} | sed 's/>/>{s} /' > {args.files_prefix}.{s}.consensus.fa")
     return f"{args.files_prefix}.{s}.consensus.fa"
 
 def get_consensus_vcf(sample: str,input_vcf: str,args: argparse.Namespace) -> str:
