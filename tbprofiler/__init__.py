55bc9feaf49c61366af64df0a77da6bb6d0519d1
Jody Phelan <jody.phelan@lshtm.ac.uk>
2024-11-13 21:51:37 +0100

    move phylo to plugin

--- tbprofiler/__init__.py
+++ tbprofiler/__init__.py
@@ -5,7 +5,17 @@ from .utils import *
 from .spoligotyping import *
 from .output import *
 from .snp_dists import *
-from .phylo import *
 from .docx import *
+from abc import ABC, abstractmethod
 
 __version__ = "6.4.0"
+
+
+class ProfilePlugin:
+    """
+    A class to define a plugin for tbprofiler
+    """
+    @abstractmethod
+    def run(self):
+        """Generic run method"""
+        pass
