2223f50c6482349d2cdcf072bef9d2a715f150aa
Jody Phelan <jody.phelan@lshtm.ac.uk>
2024-12-02 11:10:27 +0000

    remove temp file produced

--- tbprofiler/consensus.py
+++ tbprofiler/consensus.py
@@ -71,6 +71,7 @@ def get_consensus_vcf(sample: str,input_vcf: str,args: argparse.Namespace) -> st
     run_cmd(f"cat {args.conf['ref']} {consensus_file}> {tmp_aln}")
     outfile = f"{args.files_prefix}.masked.vcf"
     run_cmd(f"fa2vcf.py {tmp_aln} {outfile}")
-    os.remove(tmp_aln)
+    run_cmd(f'rm {tmp_aln} {tmp_aln}.fai')
+    
     return outfile
 
