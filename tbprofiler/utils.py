d4425102b35614181060d2ddb2cbcccb138d3257
Jody Phelan <jody.phelan@lshtm.ac.uk>
2025-03-07 08:52:51 +0000

    code

--- tbprofiler/utils.py
+++ tbprofiler/utils.py
@@ -1,11 +1,11 @@
 from collections import defaultdict
-from pathogenprofiler import Vcf
+from pathogenprofiler import Vcf, get_db
 import argparse
 import csv
 import logging
 import re
 from packaging.version import Version
-
+import sys
 
 def process_tb_profiler_args(args: argparse.Namespace) -> None:
     if args.snp_dist:
@@ -103,3 +103,19 @@ def check_db_version(db_current_version_str: str, compatible_schema_version_str:
     if db_current_version.major != compatible_schema_version.major:
         logging.error(f"Latest database schema version {db_current_version_str} is not compatible with this version of tb-profiler (requires {compatible_schema_version.major}.x.x). Please make sure you are using the latest software and database versions.")
         quit(1)
+
+
+def get_tier1_genes(dbname,dbdir=f'{sys.base_prefix}/share/tbprofiler'):
+    conf = get_db(dbdir,dbname)
+    id2name = rv2genes(conf['bed'])
+    db = conf['json_db']
+    tier1_genes = set()
+    for gene in db:
+        for var in db[gene]:
+            for ann in db[gene][var]['annotations']:
+                if ann['type']=='drug_resistance':
+                    tier1_genes.add(id2name[gene])
+    return tier1_genes
+
+def get_default_db_dir():
+    return f'{sys.base_prefix}/share/tbprofiler'
